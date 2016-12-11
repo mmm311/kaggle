79ba34399d577beb7e15d7986fb9ddf4869d7da4
mmm311 <1034930026@qq.com>
2016-12-11 14:57:21 +0800

    单张图片提取特征值

--- Leaf/FeatureExtract4.py
+++ Leaf/FeatureExtract4.py
@@ -19,7 +19,7 @@ rcParams['figure.figsize'] = (6,6)
 # ---------- I/O ------------------
 def read_img(img_no):
     ''' reads img from disk'''
-    return mpimg.imread('../LeafData/imges/' + str(img_no) +'.jpg')
+    return mpimg.imread('../LeafData/images/' + str(img_no) +'.jpg')
 
 def get_img(num):
     '''convenience function, yields random sample from 1'''
@@ -30,5 +30,66 @@ def get_img(num):
         yield img_no, preprocess(read_img(img_no))
  
 # ----------------- preprocessing --------------
-''' splits img to 0 and 255 values at threshold ''''
-def threshold(img,):       
+''' splits img to 0 and 255 values at threshold '''
+def threshold(img, threshold = 250):
+    return ((img > threshold) * 255).astype(int)
+
+def portrait(img):
+    ''' make all leaves stand straight'''
+    y , x = np.shape(img)
+    return img.transpose() if x > y else img
+
+def resample(img ,size):
+    ''' resample img to size without distorsion '''
+    ratio = size / max(np.shape(img))
+    return sp.misc.imresize(img, ratio, mode = 'L', interp = 'nearest')
+
+def fill(img, size = 500, tolerance = 0.95):  
+    ''' extends the image if it is signifficantly smaller than size'''
+    y, x = np.shape(img)
+    
+    if x <= size * tolerance:
+        pad = np.zeros((y, int ((size - x) / 2)),dtype = int)
+        img = np.concatenate((pad, img, pad) , axis = 1)
+    if y <= size * tolerance:
+        pad = np.zeros((int ((size -y ) /2 ), x), dtype = int)
+        img = np.concatenate((pad, img , pad), axis = 0)
+    return img
+# ---------------------feature enginerring---------------
+def extract_shape(img):
+    '''
+        Expects prepared image, returns leaf shape in img format.
+        The strength of smoothing had to be dynamically set
+        in order to get consistent results for different sizes.
+    '''
+    size = int(np.count_nonzero(img) / 1000)
+   
+    brush = int(5 * size / size ** 0.75)
+    # 高斯过滤
+    # ?
+    return ndi.gaussian_filter(img, sigma = brush, mode = 'nearest') > 200
+
+
+# ----------------- wrapping function -----------------
+def preprocess(img , do_portrait = True, do_resample = 500,
+               do_fill = True, do_threshold = 250):
+    if do_portrait:
+        img = portrait(img)
+    if do_resample:
+        img = resample(img, size = do_resample)
+    if do_fill:
+        img = fill(img, size = do_resample)
+    if do_threshold:
+        img = threshold(img, threshold = do_threshold)
+    return img
+
+img = read_img(1)
+title , img = list(get_img([1]))[0]
+blur = extract_shape(img)
+
+plt.imshow(img, cmap='Set3')
+blade = max(measure.find_contours(img, .8),key = len)
+shape = max(measure.find_contours(blur, 0.8), key = len)
+plt.scatter(blade[:,1],blade[:,0],s = 1,linewidths= 0,c = 'r')
+plt.scatter(shape[:,1],shape[:,0], s = 1, linewidths= 0 , c = 'k')
+plt.show()
